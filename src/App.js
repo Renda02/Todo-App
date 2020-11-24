5b7a92e9995c4b91ec8af21bdb6c56f661c1c511
Renda02 <rendaniluvhengo89@gmail.com>
2020-11-24 07:49:41 +0100

    Added useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+function App(props) {
   // Create a todos state handler to store all our todos as an array
+  const [input, setInput] = useState("");
+ 
 
   // Create a currenttodo state handler to store the current todo
 
   // Create an error state handler to help us inform the user in case there is an error
 
   const handleChange = (e) => {
-    // We capture the value of the input and set it as current todo
+    setInput(e.target.value);// We capture the value of the input and set it as current todo
   };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    /*props.onSubmit({
+    id: Math.floor(Math.random() * 1000),
+      text: input)};*/
+    setInput('');
+  };
+
+  
+    
 
   return (
     <>
-      <div class="header">Dragos's Tasks</div>
+      <div class="header">Rendani's Tasks</div>
       <div className="root">
         <div className="dashboard">
-          <p className="in-progress">{}</p>
-          <p>in progress</p>
+          <p className="n-progress">{}</p>
+          <p>In progress</p>
         </div>
         <form onSubmit={handleSubmit} className="add-todo-form">
           <div className="input-row">
